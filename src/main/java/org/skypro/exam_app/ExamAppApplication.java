151f3b7aeee34bd1fb2d20f2dbc885f0557ef386
valeriia1805 <nnn2536@mail.ru>
2025-11-10 22:17:18 +0300

    initial commit

---
+++ src/main/java/org/skypro/exam_app/ExamAppApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.exam_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamAppApplication.class, args);
+	}
+
+}
